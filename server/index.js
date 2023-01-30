471f52bf54cd5fa6cc75d4ae566fc6666e96eca3
Nikita Stankevich <Nikita.Stankevich@itechart-group.com>
2023-01-30 13:52:27 +0100

    MongoDB connection, colorized console

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const colors = require("colors");
 require("dotenv").config();
 
 const { graphqlHTTP } = require("express-graphql");
 
 const schema = require("./schemas/schema");
 
+const connectDB = require("./config/db");
+
 const port = process.env.PORT || 5000;
+
+//DataBase commection
+connectDB();
 
 const app = express();
 
