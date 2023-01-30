d2e7956822b69816a2228497ada7c30013d6d84e
Nikita Stankevich <Nikita.Stankevich@itechart-group.com>
2023-01-30 11:23:29 +0100

    Set up the Express server with GraphQL

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 
+const { grapgqlHTTP } = require("express-graphql");
+
+const schema = require("./schemas/schema");
+
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(
+  "/graphql",
+  grapgqlHTTP({
+    schema: schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, console.log(`Server started on port ${port}`));
